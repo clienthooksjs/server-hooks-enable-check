f6ab3dfd04ae15b9fa150384eb063fc8acf8439c
jieling.guan <jieling.guan@jimu.com>
2016-08-19 17:38:58 +0800

    add

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const fork = require('child_process').fork;
+
+const path = require('path');
+
+class EnableCheckPlugin {
+  constructor({ cwd = '', hookName = '' } = {}) {
+    this[Symbol.for('hookName')] = hookName;
+
+    this[Symbol.for('initProcess')]();
+  }
+
+  getProcess() {
+    return this[Symbol.for('process')];
+  }
+
+  getHookName() {
+    return this[Symbol.for('hookName')];
+  }
+
+  [Symbol.for('initProcess')]() {
+    const modulePath = path.join(__dirname, 'lib','enable-check.js');
+
+    const hookName = this.getHookName();
+
+    this[Symbol.for('process')] = fork(modulePath, [hookName], {silent: true});
+  }
+};
+
+module.exports = EnableCheckPlugin;
